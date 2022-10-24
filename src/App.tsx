20ee921d879f28bba1b89270aeb45918e965adab
John Doe <johndoe@email.com>
2022-10-24 12:51:31 -0500

    Get rid of boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+
+      <h3>Number Of Teams: 8</h3>
+      <h3>Number Of Teams: 8</h3>
     </div>
   );
 }

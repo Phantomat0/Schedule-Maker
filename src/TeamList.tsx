1bf0f9009e14c9ce1fd2f08501b15d555013eea1
John Doe <johndoe@email.com>
2022-11-01 14:34:34 -0500

    Prevent less than 2 teams

--- src/TeamList.tsx
+++ src/TeamList.tsx
@@ -1,9 +1,14 @@
 interface TeamListProps {
   teamList: string[];
   setTeamsList: React.Dispatch<React.SetStateAction<string[]>>;
+  index: number;
 }
 
-const TeamList: React.FC<TeamListProps> = ({ teamList, setTeamsList }) => {
+const TeamList: React.FC<TeamListProps> = ({
+  teamList,
+  setTeamsList,
+  index,
+}) => {
   const onInputChange = (e: any) => {
     const indexOfTeam = parseInt(
       e.target.attributes.getNamedItem("data-team-id").value
@@ -25,6 +30,8 @@ const TeamList: React.FC<TeamListProps> = ({ teamList, setTeamsList }) => {
 
     let teams = teamList;
 
+    if (teams.length === 2) return null;
+
     teams.splice(indexOfTeam, 1);
 
     setTeamsList([...teams]);
@@ -38,25 +45,38 @@ const TeamList: React.FC<TeamListProps> = ({ teamList, setTeamsList }) => {
   };
 
   return (
-    <div className="team-list-wrapper">
-      <ul>
+    <div>
+      <h3>Divison {index}</h3>
+      <ul className="team-list">
         {teamList.map((team, index) => {
           return (
-            <li>
+            <li key={team + index}>
               <input
                 type="text"
                 value={teamList[index]}
+                className="input-remove-default"
                 data-team-id={index}
                 onChange={onInputChange}
               />
-              <button data-team-id={index} onClick={handleDeleteTeam}>
-                -
-              </button>
+              <div className="team-del-bttn-wrapper">
+                <button
+                  className="input-remove-default team-del-bttn"
+                  data-team-id={index}
+                  onClick={handleDeleteTeam}
+                >
+                  -
+                </button>
+              </div>
             </li>
           );
         })}
         <li>
-          <button onClick={handleAddNewTeam}>Add Team</button>
+          <button
+            className="input-remove-default team-add-bttn"
+            onClick={handleAddNewTeam}
+          >
+            Add Team
+          </button>
         </li>
       </ul>
     </div>

fd01b373f0c371609be73cf0acb1fd4cfea409ab
John Doe <johndoe@email.com>
2022-11-01 14:34:23 -0500

    date formatting

--- src/utilts.ts
+++ src/utilts.ts
@@ -16,6 +16,23 @@ export const shuffleArray = <T>(arr: T[]): T[] => {
   return arr;
 };
 
+/**
+ * Get date formatted i.e November 3rd 2019
+ */
+export const formatDate = (
+  dateTime: number | Date = Date.now(),
+  options?: Intl.DateTimeFormatOptions | null
+): string => {
+  if (!options) {
+    options = {
+      dateStyle: "medium",
+      timeStyle: "short",
+      timeZone: "America/Chicago",
+    };
+  }
+  return new Date(dateTime).toLocaleString("en-US", options);
+};
+
 export const splitAt = (i: number, arr: any[]) => {
   const clonedArray = [...arr];
   return [clonedArray.splice(0, i), clonedArray];
@@ -27,4 +44,22 @@ export const innerConcat = (arr1: any[], arr2: any[]) => {
     const correctIndex = Math.floor(index / 2);
     return arrToGet[correctIndex];
   });
+};
+
+export const daysDiff = (dt1: Date, dt2: Date) => {
+  // calculate the time difference of two dates JavaScript
+  var diffTime = dt2.getTime() - dt1.getTime();
+
+  // calculate the number of days between two dates javascript
+  var daysDiff = diffTime / (1000 * 3600 * 24);
+
+  return Math.floor(daysDiff);
+};
+
+export const datesAreOnSameDay = (date1: Date, date2: Date) => {
+  return (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate()
+  );
 };
